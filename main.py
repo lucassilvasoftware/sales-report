a839b06eaaa6b382405ff169360b888dbe151c5f
lucassilvasoftware <54152996+lucassilvasoftware@users.noreply.github.com>
2021-08-19 15:20:36 -0300

    updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 # import the database
-sales_table = pd.read_excel('"./Excel spreadsheets/sales.xlsx')
+sales_table = pd.read_excel('./excel spreadsheet/sales.xlsx')
 
 print(sales_table)
