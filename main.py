afa04920baa674b49a8c1e49c94af6ce0739cb26
lucassilvasoftware <54152996+lucassilvasoftware@users.noreply.github.com>
2021-08-19 15:33:58 -0300

    updated main.py

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ import pandas as pd
 # import the database
 sales_table = pd.read_excel('./excel spreadsheet/sales.xlsx')
 
+# view database
+pd.set_option('display.max_columns', None)
 print(sales_table)
+
+# revenues
+revenues = sales_table[['Store ID']]
