600048d409e06fb10f3330f837315179ef2a4287
lucassilvasoftware <54152996+lucassilvasoftware@users.noreply.github.com>
2021-08-20 15:35:42 -0300

    updated main.py

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ revenues = sales_table[['Store ID', 'Final value']].groupby('Store ID').sum()
 print(revenues)
 
 # number of products sold per store
-amount_sales = sales_table[['Store ID', 'Amount']].groupby('Store ID')
+amount_sales = sales_table[['Store ID', 'Amount']].groupby('Store ID').sum()
 print(amount_sales)
 
 print('-' * 50)
 # average ticket per product
-average_ticket = revenues['Final value'] / amount_sales['Amount']
+average_ticket = (revenues['Final value'] / amount_sales['Amount']).to_frame
+average_ticket = average_ticket.rename(columns={0: 'Ticket Médio'})
 print(average_ticket)
