c7cd8f28a6bb22abd6898951fc64bd2489e50343
lucassilvasoftware <54152996+lucassilvasoftware@users.noreply.github.com>
2021-09-10 20:49:33 -0300

    updated main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ print(average_ticket)
 # send email with report
 outlook = win32.Dispatch('outlook.application')
 mail = outlook.CreateItem(0)
-mail.To = '54152996+lucassilvasoftware@users.noreply.github.com'
+mail.To = 'X'
 mail.Subject = 'Sales Report'
 mail.HTMLBody = f'''
 <p>Below is the sales report for each store.<p>
